4f6a25b4a4eb28c218b1a3bbd67e0bac7d171b8a
Jordi Adan <jordiadan@gmail.com>
2018-11-05 20:27:07 +0100

    First commit

---
+++ src/main/java/com/gymprogress/GymprogressApplication.java
@@ -0,0 +1,12 @@
+package com.gymprogress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymprogressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymprogressApplication.class, args);
+    }
+}
